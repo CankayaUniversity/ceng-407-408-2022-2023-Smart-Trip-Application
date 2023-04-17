06ce8423db427789d980232a209425aa23180fc1
BasakUnal <bsk.unal57@gmail.com>
2023-04-18 00:21:32 +0300

    Closes #24 -> Testing frontend and backend connection.

--- SmartTrip/src/app/app.component.ts
+++ SmartTrip/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) {
+    console.log(environment.value);
+  }
+
+  async ngOnInit(): Promise<void> {
+    const locations = await firstValueFrom(
+      this.http.get(`${environment.serverRoot}/locations`)
+    );
+    console.log(locations);
+  }
+
 }
