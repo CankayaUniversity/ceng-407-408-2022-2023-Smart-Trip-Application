9313858b665083393204ef320f13d30137c46a7f
BasakUnal <bsk.unal57@gmail.com>
2023-04-22 11:59:49 +0300

    Closes #29 -> Editing the write review page.

--- SmartTrip/src/app/write-review/write-review.page.ts
+++ SmartTrip/src/app/write-review/write-review.page.ts
@@ -32,4 +32,41 @@ export class WriteReviewPage implements OnInit {
     const { data, role } = await modal.onWillDismiss();
   }
 
+  public toilet = 'assets/icon/toilet.png';
+  public disabled = 'assets/icon/disabled.png';
+  public babycare = 'assets/icon/babycare.png';
+  public mosque = 'assets/icon/mosque.png';
+  selectToilet(){
+    if(this.toilet=='assets/icon/toilet.png')
+    {
+      this.toilet='assets/icon/toiletWhite.png';
+    }else{
+      this.toilet = 'assets/icon/toilet.png';
+    }
+  }
+  selectDisabled(){
+    if(this.disabled=='assets/icon/disabled.png')
+    {
+      this.disabled='assets/icon/disabledWhite.png';
+    }else{
+      this.disabled = 'assets/icon/disabled.png';
+    }
+  }
+  selectBabycare(){
+    if(this.babycare=='assets/icon/babycare.png')
+    {
+      this.babycare='assets/icon/babycareWhite.png';
+    }else{
+      this.babycare = 'assets/icon/babycare.png';
+    }
+  }
+  selectMosque(){
+    if(this.mosque=='assets/icon/mosque.png')
+    {
+      this.mosque='assets/icon/mosqueWhite.png';
+    }else{
+      this.mosque = 'assets/icon/mosque.png';
+    }
+  }
+
 }
