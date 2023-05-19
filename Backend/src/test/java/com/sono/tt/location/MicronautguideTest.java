00f0d94fa10039080b401de5d99168895fc2c3f5
BasakUnal <bsk.unal57@gmail.com>
2023-05-19 13:53:33 +0300

    Backend code start

---
+++ Backend/src/test/java/com/sono/tt/location/MicronautguideTest.java
@@ -0,0 +1,21 @@
+package com.sono.tt.location;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronautguideTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
