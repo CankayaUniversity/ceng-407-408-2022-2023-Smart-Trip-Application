6a60852cfbe68c476d17b9150f2cf6f80370c9a7
Münteha Yavuz <110225035+muntehayvz@users.noreply.github.com>
2023-06-08 23:23:51 +0300

    build files changed

--- SmartTrip/src/environments/environment.prod.ts
+++ SmartTrip/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  google_maps_api_key: '',
-  serverRoot: 'https://apigateway...',
+  google_maps_api_key: 'AIzaSyDxCcq_rhwetsv6t0hdy6giyJ41wN',
+  serverRoot: 'http://192.168.1.175:8080',
   value: 1
 };
